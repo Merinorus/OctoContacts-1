41dd57102e3a43dc991bd7bdd162c289f9661afb
Merinorus <merinorus.antoine@gmail.com>
2014-02-24 18:15:09 +0100

    Reprise du code original de Zorg

---
+++ OctoContacts/src/com/example/aixproject/UtilisateurBDD.java
@@ -0,0 +1,24 @@
+package com.example.aixproject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class UtilisateurBDD {
+	public static final String TABLE_UTILISATEURS = "table_utilisateurs";
+	public static final String COL_ID = "UtilisateurId";
+	public static final String COL_PRENOM = "Prenom";
+	public static final String COL_NOM = "Nom";
+	
+	private SQLiteDatabase bdd;
+	
+	private BaseDonnee maBaseSQLite;
+	
+	public UtilisateurBDD(Context context){
+	//On créer la BDD et sa table
+	maBaseSQLite = new BaseDonnee(context);
+	}
+
+	public void open() {
+		bdd = maBaseSQLite.getWritableDatabase();
+	}
+}
